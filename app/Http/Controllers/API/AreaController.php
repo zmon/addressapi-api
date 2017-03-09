b4c58bfde6d3972535a82c3b9b293dfc5c38dda1
gregbonner <greg.d.bonner@gmail.com>
2017-03-08 21:57:48 -0600

    Moving controllers to folder for API

--- app/Http/Controllers/API/AreaController.php
+++ app/Http/Controllers/API/AreaController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
-    //
+  /**
+   * Display a listing of the resource
+   *
+   * @return Response
+   */
+    public function index()
+    {
+        $users = DB::select('select * from users where active = ?', [1]);
+        return view('user.index', ['users' => $users]);
+    }
+
+    public function getById()
+    {
+        return array(
+          1 => "John",
+          2 => "Mary",
+          3 => "Steven"
+        );
+    }
 }
